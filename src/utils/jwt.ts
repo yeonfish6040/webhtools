c5356611ac72d3af817be8f968addd7bed8f7e69
Yeonjun Lee <yeonfish6040@gmail.com>
2025-04-01 08:32:50 +0900

    jwt update

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -2,8 +2,7 @@ import * as crypto from "crypto";
 
 import {JWTBody, JWTHeader} from "./types";
 
-// export const JWT = (header: JWTHeader, body: JWTBody, key: string = "") => {
-export const JWT = (header, body, key = "") => {
+export const JWT = (header: JWTHeader, body: JWTBody, key: string = "") => {
   const headerBase64 = Buffer.from(JSON.stringify(header)).toString("base64url");
   const bodyBase64 = Buffer.from(JSON.stringify(body)).toString("base64url");
   const withoutHash = `${headerBase64}.${bodyBase64}`;
