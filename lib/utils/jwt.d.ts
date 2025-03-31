429a3f6476a772029da1d94bc376f3991ba2fde9
Yeonjun Lee <yeonfish6040@gmail.com>
2025-04-01 08:32:40 +0900

    jwt update

---
+++ lib/utils/jwt.d.ts
@@ -0,0 +1,1 @@
+export declare const JWT: (header: any, body: any, key?: string) => string;
--- lib/utils/types.d.ts
+++ lib/utils/types.d.ts
@@ -3,3 +3,9 @@ export type ConfigKey = (typeof ConfigKeyValues)[number];
 export type Config = {
     [key in ConfigKey | string]: any;
 };
+export type JWTHeader = {
+    [key: string]: string;
+};
+export type JWTBody = {
+    [key: string]: any;
+};
